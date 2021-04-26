5869ba639cada681a3a08b0f23c47c75303d772c
ErinCleaver-GRCC <77419665+ErinCleaver-GRCC@users.noreply.github.com>
2021-04-26 15:34:22 -0400

    update 3-34-2021 3:34

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        Testing
-      </header>
+    <div>
+      My app
     </div>
   );
 }
